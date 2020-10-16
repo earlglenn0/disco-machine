21fb4cc5f35d93513c393250421e7ccdb8308a1e
garyamaro1 <gary@dnamicro.com>
2020-10-16 09:21:36 +0800

    remove http

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const app = require('express')()
-const http = require('http').createServer(app)
 const { formatState } = require('./utils')
 const config = require('./config')
 const kafka = require('kafka-node')
@@ -18,42 +16,36 @@ const { Machine, interpret } = require('xstate')
 const discoConfig = require('./discoConfig')
 const discoImplementation = require('./discoImplementation')
 
+const service  = interpret(Machine(discoConfig, discoImplementation))
+service.start()
 
-
-
-http.listen(3002, () => {
-  console.log('listening on *:3002');
-  const service  = interpret(Machine(discoConfig, discoImplementation))
-  service.start()
-
-  const produceEvent = (topic, payload) => {
-    const payloads = [{
-      topic,
-      messages: [ payload]
-    }]
-    producer.send(payloads, (err, data) => {
-      if (err) {
-        console.log('producer failed')
-      } else {
-        console.log('producer succeeded')
-      }
-    })
-  }
-  
-  producer.on('ready', () => {
-    console.log('producer ready')
-    service.onTransition(state => {
-      const payload = {
-        ...formatState(state.value),
-        ...state.context,
-        isBroken: state.value === 'broken'
-      }
-      produceEvent(config.kafka_topic_produce, JSON.stringify(payload))
-    })
-  
-    consumer.on('message', (message) => {
-      console.log({ message })
-      service.send(message.value)
-    })
+const produceEvent = (topic, payload) => {
+  const payloads = [{
+    topic,
+    messages: [ payload]
+  }]
+  producer.send(payloads, (err, data) => {
+    if (err) {
+      console.log('producer failed')
+    } else {
+      console.log('producer succeeded')
+    }
   })
-});
+}
+
+producer.on('ready', () => {
+  console.log('producer ready')
+  service.onTransition(state => {
+    const payload = {
+      ...formatState(state.value),
+      ...state.context,
+      isBroken: state.value === 'broken'
+    }
+    produceEvent(config.kafka_topic_produce, JSON.stringify(payload))
+  })
+
+  consumer.on('message', (message) => {
+    console.log({ message })
+    service.send(message.value)
+  })
+})
